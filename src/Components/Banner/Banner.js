c411e4a1eaccdab50578dcb42d64f93121ce950d
Viswajith2003 <viswajithviswa715@gmail.com>
2024-12-04 22:52:51 +0530

    modify the all design

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,31 +1,114 @@
-import React, { useEffect, useState} from 'react'
-import {API_KEY,imageUrl} from '../../constant/constant'
-import axios from '../../axios';
-import './Banner.css'
+import React, { useEffect, useState } from "react";
+import { API_KEY, imageUrl } from "../../constant/constant";
+import axios from "../../axios";
+import "./Banner.css";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function Banner() {
-  const [movie,setMovie]=useState()
-  useEffect(()=>{
-    axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
-          console.log(response.data.results[17])
-          setMovie(response.data.results[3])
-    })
-  },[])
+  const [movie, setMovie] = useState();
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`
+      )
+      .then((response) => {
+        console.log(response.data.results[17]);
+        setMovie(
+          response.data.results[
+            Math.floor(Math.random() * response.data.results.length)
+          ]
+        );
+      });
+  }, []);
+  useEffect(() => {
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".banner",
+        start: "top center", // Adjust trigger start
+        toggleActions: "play none none reverse", // Animation actions
+      },
+    });
+
+    // Banner fade-in
+    tl.to(".banner", {
+      opacity: 1,
+      duration: 1.5,
+      y: 0, // Reset translateY
+      ease: "power2.inOut",
+    });
+
+    // Content animation
+    tl.to(
+      ".content",
+      {
+        opacity: 1,
+        x: 0, // Reset translateX
+        duration: 1,
+        ease: "power2.inOut",
+      },
+      "-=1" // Overlap timing
+    );
+
+    // Buttons stagger animation
+    tl.to(
+      ".banner_btn button",
+      {
+        opacity: 1,
+        scale: 1, // Reset scale
+        duration: 0.5,
+        ease: "power2.inOut",
+        stagger: {
+          each: 0.3,
+        },
+      },
+      "-=0.5" // Overlap timing
+    );
+
+    // Description fade-in
+    tl.to(
+      ".description",
+      {
+        opacity: 1,
+        duration: 1,
+        ease: "power2.inOut",
+      },
+      "-=0.5" // Overlap timing
+    );
+  }, []);
+
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCounter((prevCount) => prevCount + 1);
+      if (counter === 8) {
+        setCounter(0);
+      }
+    }, 5000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [counter]);
   return (
-    <div 
-   style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path :""})`}}
-    className='banner'>
-        <div className='content'>
-           <h1 className='title'>{movie ? movie.title :""}</h1>
-            <div className='banner_btn'>
-                <button className='btn'>Play</button>
-                <button className='btn'>My list</button>
-            </div>
-            <h1 className='description'>{movie ? movie.overview :""}</h1>
+    <div
+      style={{
+        backgroundImage: `url(${movie ? imageUrl + movie.backdrop_path : ""})`,
+      }}
+      className="banner"
+    >
+      <div className="content">
+        <h1 className="title">{movie ? movie.title : ""}</h1>
+        <div className="banner_btn">
+          <button className="btn">Play</button>
+          <button className="btn">My list</button>
         </div>
-        <div className="fade_bottom"></div>
+        <h1 className="description">{movie ? movie.overview : ""}</h1>
+      </div>
+      <div className="fade_bottom"></div>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,55 +1,96 @@
-import './RowPost.css'
-import axios from '../../axios'
-import React,{useEffect,useState} from 'react'
-import {imageUrl,API_KEY} from '../../constant/constant'
-import YouTube from 'react-youtube'
+import "./RowPost.css";
+import axios from "../../axios";
+import React, { useEffect, useState } from "react";
+import { imageUrl, API_KEY } from "../../constant/constant";
+import YouTube from "react-youtube";
+import { gsap } from "gsap";
 
 function RowPost(props) {
-  const [movies, setMovies] = useState([])
-  const [urlId,setUrlId] =useState('')
+  const [movies, setMovies] = useState([]);
+  const [urlId, setUrlId] = useState("");
+
   useEffect(() => {
-    axios.get(props.url).then(response=>{
-      console.log(response.data)
-      setMovies(response.data.results)
-    }).catch(err=>{
-      alert("Network error")
-    })
-  }, [])
+    // Fetch movies
+    axios
+      .get(props.url)
+      .then((response) => {
+        console.log(response.data);
+        setMovies(response.data.results);
+        // Animate posters once they are fetched
+        gsap.from(".poster", {
+          opacity: 0,
+          scale: 0.9,
+          duration: 0.5,
+          stagger: 0.1, // Animates each poster sequentially
+        });
+      })
+      .catch((err) => {
+        alert("Network error");
+      });
+  }, [props.url]);
 
   const opts = {
-    height: '390',
-    width: '100%',
+    height: "390",
+    width: "100%",
     playerVars: {
-      // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
-    }
+    },
   };
-  const handleMovie=(id)=>{
-    console.log(id)
-    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
-      if(response.data.results.length!==0){
-        setUrlId(response.data.results[0])
-      }else{
-        console.log('Array empty')
+
+  const handleMovie = (id) => {
+    console.log(id);
+    axios
+      .get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`)
+      .then((response) => {
+        if (response.data.results.length !== 0) {
+          setUrlId(response.data.results[0]);
+        } else {
+          console.log("Array empty");
+        }
+      });
+  };
+
+  // Auto horizontal scrolling
+  useEffect(() => {
+    const postersContainer = document.querySelector(".posters");
+    let scrollAmount = 0;
+    const interval = setInterval(() => {
+      if (postersContainer) {
+        // Scroll 5px to the right
+        postersContainer.scrollLeft += 5;
+        scrollAmount += 5;
+
+        // Reset scrolling when reaching the end
+        if (
+          scrollAmount >=
+          postersContainer.scrollWidth - postersContainer.offsetWidth
+        ) {
+          scrollAmount = 0;
+          postersContainer.scrollLeft = 0;
+        }
       }
-     // console.log(response.data)
-    })
-  }
-  
+    }, 50); // Adjust speed here (lower is faster)
+
+    return () => clearInterval(interval); // Cleanup the interval
+  }, []);
+
   return (
-    <div className='row'>
+    <div className="row">
       <h3>{props.title}</h3>
-        <div className='posters'>
-          {movies.map((obj)=>
-
-            <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ? 'smallPoster' : 'poster' }src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
-          )}
-
-        </div>
-     {  urlId &&  <YouTube opts={opts} videoId={urlId.key}/>  }
-        
+      <div className="posters">
+        {movies.map((obj) => (
+          <img
+            key={obj.id}
+            onClick={() => handleMovie(obj.id)}
+            className={props.isSmall ? "smallPoster" : "poster"}
+            src={`${imageUrl + obj.backdrop_path}`}
+            alt="poster"
+          />
+        ))}
+      </div>
+      {urlId && <YouTube opts={opts} videoId={urlId.key} />}
     </div>
-  )
+  );
 }
 
-export default RowPost
+export default RowPost;
